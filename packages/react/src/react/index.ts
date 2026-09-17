4654ed8b4a871ff229e674ae6d1c67fc3f56b0f3
selub <seport24@list.ru>
2026-09-17 18:37:43 +0300

    chore: generate documentation in dedicated foldes for each package

--- packages/react/src/react/index.ts
+++ packages/react/src/react/index.ts
@@ -1,7 +1,10 @@
 export { Me2emProvider } from './Me2emProvider.js';
 export { useMe2emContext } from './context.js';
+export type { Me2emContextValue } from './context.js';
 export { useCreateIdentity } from './useCreateIdentity.js';
+export type { UseCreateIdentityResult } from './useCreateIdentity.js';
 export { useImportSeed } from './useImportSeed.js';
+export type { UseImportSeedResult } from './useImportSeed.js';
 export { useHandle } from './useHandle.js';
 export { useSubHandle } from './useSubHandle.js';
 export { SeedPhraseDisplay } from './SeedPhraseDisplay.js';
